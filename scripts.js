8c008774f762ab75e20192f927e3bf36f5aa4159
Paweł Michalik <p.michalik@clearcode.cc>
2020-02-06 13:42:50 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var submitButton = document.getElementById('submit-button');
+submitButton.onclick = function() {
+  alert("You submitted the form!");
+  return false;
+};
+
+var buttons = document.getElementsByClassName('button');
+for(var z = 0; z < buttons.length; z++) {
+  var elem = buttons[z];
+  elem.onclick = function() {
+    alert("You clicked a button!");
+    return false;
+  };
+}
